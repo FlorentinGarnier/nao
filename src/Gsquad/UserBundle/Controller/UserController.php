5bc90650d5ddfc4c519e0b412e116a33fed0ba67
florentingarnier <garnier.florentin@gmail.com>
2016-11-07 17:38:39 +0100

    surcharge fosuserbundle en cours

---
+++ src/Gsquad/UserBundle/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florentingarnier
+ * Date: 07/11/2016
+ * Time: 13:51
+ */
+
+namespace Gsquad\UserBundle;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class UserController extends Controller
+{
+
+}
