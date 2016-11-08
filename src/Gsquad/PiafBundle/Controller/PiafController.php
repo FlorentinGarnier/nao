e8f33a2b4a09d14804caa92cfd2a8e9018877320
Pepert <playpero@hotmail.com>
2016-11-08 11:22:48 +0100

    Mise en place autocompletion

--- src/Gsquad/PiafBundle/Controller/PiafController.php
+++ src/Gsquad/PiafBundle/Controller/PiafController.php
@@ -5,6 +5,8 @@ namespace Gsquad\PiafBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PiafController extends Controller
 {
@@ -16,5 +18,47 @@ class PiafController extends Controller
         return $this->render('search/search.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/ajax/autocomplete/update/data", name="ajax_autocomplete_countries")
+     */
+    public function updateDataAction(Request $request)
+    {
+        $data = $request->get('input');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $connection = $em->getConnection();
+
+
+        $results = $connection->fetchAll(
+            'SELECT NOM_VERN FROM taxref'
+        );
+
+        //$results = ['abricot','abri','abruti','boudin'];
+        $temp = [];
+
+        $speciesList = '<ul id="matchList">';
+        foreach ($results as $result) {
+            if(!in_array($result, $temp)) {
+                $temp[] = $result;
+                $pos = strpos($this->removeAccents($result['NOM_VERN']),($this->removeAccents($data)));
+
+                if($pos !== false) {
+                    $matchStringBold = preg_replace('/('.$data.')/i', '<strong>$1</strong>', $result['NOM_VERN']); // Replace text field input by bold one
+                    $speciesList .= '<li id="'.$result['NOM_VERN'].'">'.$matchStringBold.'</li>'; // Create the matching list - we put maching name in the ID too
+                }
+            }
+        }
+        $speciesList .= '</ul>';
+
+        $response = new JsonResponse();
+        $response->setData(array('speciesList' => $speciesList));
+        return $response;
+    }
+
+    private function removeAccents($string) {
+        return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', preg_replace('~&([a-z]{1,2})(acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8'))), ' '));
     }
 }
