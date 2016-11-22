d217d571edb56a2c8f3e4bbd92f17d4d396fd942
florentingarnier <garnier.florentin@gmail.com>
2016-11-23 00:21:59 +0100

    ajout des bouton google et facebook

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,22 +16,14 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Gsquad\CoreBundle\GsquadCoreBundle(),
-<<<<<<< HEAD
-=======
-            new \FOS\UserBundle\FOSUserBundle(),
             new Gsquad\BlogBundle\GsquadBlogBundle(),
             new \Ivory\CKEditorBundle\IvoryCKEditorBundle(),
->>>>>>> origin/develop
             new Gsquad\AdminBundle\GsquadAdminBundle(),
             new Gsquad\PiafBundle\GsquadPiafBundle(),
-<<<<<<< HEAD
             new \HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
-            new \FOS\UserBundle\FOSUserBundle(),
-=======
             new Gsquad\UserBundle\GsquadUserBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
-            new \HWI\Bundle\OAuthBundle\HWIOAuthBundle()
->>>>>>> origin/develop
+            new \FOS\UserBundle\FOSUserBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -52,16 +44,16 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
