3ed4292bac6682dd257a58a83288841cb5ea5ed7
florentingarnier <garnier.florentin@gmail.com>
2016-12-07 17:40:42 +0100

    landing page

---
+++ src/Gsquad/CoreBundle/Tests/Controller/MarketingControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Gsquad\CoreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MarketingControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/participez-etude-observation-oiseaux');
+    }
+
+}
