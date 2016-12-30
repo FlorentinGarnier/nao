22950d6785fa4011af4715b40c743e6fe38d22a1
Aurore Gaucher <gaucher.aurore@laposte.net>
2016-12-30 14:00:56 +0100

    admin users

--- src/Gsquad/AdminBundle/Controller/AdminUsersController.php
+++ src/Gsquad/AdminBundle/Controller/AdminUsersController.php
@@ -8,13 +8,17 @@
 
 namespace Gsquad\AdminBundle\Controller;
 
+use Gsquad\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminUsersController extends Controller
 {
     /**
      * @Route("/users", name="admin_users")
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function adminUsersAction()
     {
@@ -28,7 +32,43 @@ class AdminUsersController extends Controller
 
         return $this->render('admin/users/users.html.twig', array(
             'users' => $users,
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * @Route("/users/delete/{id}", name="user_delete")
+     * @Security("has_role('ROLE_SUPER_ADMIN')")
+     */
+    public function deleteUserAction(Request $request, User $user)
+    {
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+
+            $this->addFlash('info', "L'utilisateur a été supprimé.");
+        }
+        return $this->redirectToRoute('admin_users');
+    }
+
+    /**
+     * @Route("/users/edit/{id}", name="user_edit")
+     */
+    public function editUserAction(User $user, Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $newRoles = [
+                $request->request->get('new_role_site'),
+                $request->request->get('new_role_blog')
+            ];
+            $user->setRoles($newRoles);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash('info', "L'utilisateur a été modifié.");
+        }
+        return $this->redirectToRoute('admin_users');
     }
 }
